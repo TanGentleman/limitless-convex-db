250dd1d16a8cbb0c887a7a0395282b24133db80e
Tanuj <Tanuj@cmu.edu>
2025-03-18 01:07:55 -0700

    Create __init__.py

---
+++ lifelog/__init__.py
@@ -0,0 +1,8 @@
+"""
+Lifelog - A tool for managing and summarizing lifelogs
+"""
+
+__version__ = "0.1.0"
+
+from .core.data_manager import LifelogDataManager
+from .core.api_client import LifelogAPIClient 
