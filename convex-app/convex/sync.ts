847b72b8173c2dae65adba67b99790337abd4e4b
Tanuj <Tanuj@cmu.edu>
2025-03-23 05:56:18 -0700

    Make progress on sync functionality

--- convex-app/convex/sync.ts
+++ convex-app/convex/sync.ts
@@ -1,13 +1,23 @@
 import { v } from "convex/values";
 import { internal } from "./_generated/api";
 import { internalAction, } from "./_generated/server";
-export const syncLifelogs = internalAction({
+export const syncLimitless = internalAction({
     args: {
         startTime: v.number(),
     },
     handler: async (ctx, args) => {
-        // 1. Call internal.meta.read to get meta
+        // 1. Call internal.metadata.getLatest to get meta
+        const meta = await ctx.runQuery(internal.metadata.getLatest);
+        if (meta.length === 0) {
+            console.log("No metadata found, creating default");
+            const metaId = await ctx.runMutation(internal.metadata.createDefaultMeta);
+            if (metaId === null) {
+                console.log("Failed to create default metadata! Aborting sync.");
+                return null;
+            }
+        }
         // 2. Fetch lifelogs from Limitless API
+        
         // 3. Dedupe and (optional) embed markdowns (new table)
         // 4. Add deduped lifelogs to database
         // 5. Update meta
