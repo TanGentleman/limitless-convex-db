b6f6ce268a2809f209b0d1ab79a6358bcc919c65
Tanuj <Tanuj@cmu.edu>
2025-03-25 13:30:50 -0700

    deprecate more python files

--- lifelog/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Lifelog - A tool for managing and summarizing lifelogs
-"""
-
-__version__ = "0.1.0"
-
-from .core.data_manager import LifelogDataManager
-from .core.api_client import LifelogAPIClient 
